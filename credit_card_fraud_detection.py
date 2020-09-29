0984b930ad0414c8ca5ea56397127cb2e53fb0e4
Paras Gulati <44550739+parasgulati@users.noreply.github.com>
2020-09-29 13:41:20 +0530

    Update credit_card_fraud_detection.py

--- credit_card_fraud_detection.py
+++ credit_card_fraud_detection.py
@@ -44,7 +44,7 @@ yData = Y.values # removing column headers of Y
 
 from sklearn.model_selection import train_test_split 
 # Split the data into training and testing sets 
-xTrain, xTest, yTrain, yTest = train_test_split(xData, yData, test_size = 0.2, random_state = 42) # train:test = 70:30
+xTrain, xTest, yTrain, yTest = train_test_split(xData, yData, test_size = 0.2, random_state = 42) # train:test = 80:20
 
 # Building the Random Forest Classifier (RANDOM FOREST) 
 from sklearn.ensemble import RandomForestClassifier 
